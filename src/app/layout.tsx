68352021ab6bc03ccef9becc0640d08a27751d0f
Ming Chia <mdchia@users.noreply.github.com>
2026-06-01 22:23:30 +1000

    Remove link refs for APPN deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,25 +35,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="canonical" href="https://www.fileglance.info" />
-        <Script id="matomo" strategy="afterInteractive">
-          {process.env.NODE_ENV === "production"
-            ? `
-                var _paq = window._paq = window._paq || [];
-                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-                _paq.push(['disableCookies']);
-                // _paq.push(['trackPageView']);
-                _paq.push(['enableLinkTracking']);
-                (function() {
-                  var u="https://piwik.mdell.org/";
-                  _paq.push(['setTrackerUrl', u+'matomo.php']);
-                  _paq.push(['setSiteId', '10']);
-                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-                })();
-                `
-            : ""}
-        </Script>
       </head>
       <body
         className={`${roboto.className} ${roboto.variable} ${roboto_mono.variable}`}
